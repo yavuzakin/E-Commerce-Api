cb9e86e1f335f52ae8d2f37297d86cebb651a9c7
yavuzakinn <17050111035@ybu.edu.tr>
2021-10-04 10:21:45 +0300

    Models and routes are created

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
+const userRoute = require('./routes/users');
+const productRoute = require('./routes/products');
+const orderRoute = require('./routes/orders');
+const cartRoute = require('./routes/carts');
+const authRoute = require('./routes/auth');
+
+// To be able to use dotenv file
+dotenv.config();
+
+// To be able to take json objects inside request body
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log(`DB connection is successfull`))
+    .catch(err => console.log(err))
+
+// middleware
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/auth', authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Listening port number ${process.env.PORT}`);
+})
