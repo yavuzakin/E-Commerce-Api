82307e86d307cf18cb1385792d19f29de1b1fedd
yavuzakinn <17050111035@ybu.edu.tr>
2021-10-04 16:01:19 +0300

    User registration and login processes are added to project. crypto-js package is also added to project for encryption of password.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,42 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
+// Register
+router.post('/register', async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET_KEY).toString()
+    });
 
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        // Get user from DB by username
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json('Bad credentials');
+
+        // Decrypt the password
+        const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET_KEY).toString(CryptoJS.enc.Utf8);
+
+        // Check if passwords match ?
+        decryptedPassword !== req.body.password && res.status(401).json('Bad credentials');
+
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
